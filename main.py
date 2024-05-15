24385735b78360dd0a82eac5ee5c7e0562c72d28
Luis Guilherme de Souza Munhoz <luisguilherme.sm@hotmail.com>
2024-05-15 16:43:37 -0300

    Testes convertendo pdf pra texto

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from typing import Dict
+from typing import Any, Dict
+from pdfquery import PDFQuery
 from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 
 @app.post("/upload/")
-async def create_upload_file(file: UploadFile) -> Dict[str, str]:
-    return {"filename": file.filename}
+async def create_upload_file(file: UploadFile) -> Dict[str, Any]:
+    if not file.filename or not file.filename.endswith(".pdf"):
+        raise ValueError("Arquivo inválido! É necessário um arquivo pdf")
+    pdf = PDFQuery(file.file)
+    pdf.load()
+    textos = "\n".join(pdf.tree.xpath("//text()"))
+    with open(file.filename.replace(".pdf", ".txt"), "wb") as converted_file:
+        converted_file.write(textos.encode())
+    return {"info": "Arquivo convertido com sucesso!"}
